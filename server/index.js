8d78019825fdf6fdbcdb264b20787f4484fac7e3
bkoimett <benkoimet@gmail.com>
2025-10-21 19:33:17 +0300

    feat: create GET all book route

--- server/index.js
+++ server/index.js
@@ -13,6 +13,16 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// routes
+app.get('/api/books', async (req,res) => {
+    try {
+        const data = await Book.find();
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: "An error occured while fetching books."});
+    }
+})
+
 app.get("/", (req, res) => {
   res.json("Hello Mate");
 });
