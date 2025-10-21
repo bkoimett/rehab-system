ec4806cdb692000502fd1e9ac52825b97cd9c68b
bkoimett <benkoimet@gmail.com>
2025-10-21 17:28:20 +0300

    feat: add middleware ( cors, urlencoded, json )

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+// middleware
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.json("Hello Mate");
 });
