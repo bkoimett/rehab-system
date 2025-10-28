15dd9461010c407f9e3c997048336ef9bcd73885
bkoimett <benkoimet@gmail.com>
2025-10-28 17:48:08 +0300

    feat(client): implement book creation functionality in CreateBook component

--- client/src/routes/Book/createBook.jsx
+++ client/src/routes/Book/createBook.jsx
@@ -5,6 +5,31 @@ function CreateBook() {
   const [title, setTitle] = useState("");
   const [slug, setSlug] = useState("");
 
+  const createBook = async (e) => {
+    e.preventDefault();
+    console.table({ title, slug });
+
+    try {
+      const response = await fetch("http://localhost:8000/api/books", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          title: title,
+          slug: slug,
+        }),
+      });
+
+      if (response.ok) {
+        setTitle("");
+        setSlug("");
+      } else {
+        console.log("Failed to Submit data.");
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  };
+
   return (
     <div>
       <h1>Create Book</h1>
@@ -13,31 +38,31 @@ function CreateBook() {
         data below is pulled from a MongoDB database.
       </p>
 
-      <form className="bookdetails" >
+      <form className="bookdetails" onSubmit={createBook}>
         <div className="col-1">
           <label htmlFor="">Upload Thumbnail</label>
           <img src={NoImageSelected} alt="preview image" />
           <input type="file" accept="image/gif, image/jpeg, image/png" />
-
         </div>
         <div className="col-2">
-            <div>
-                <label htmlFor="">Title</label>
-                <input 
-                type="text" 
-                value={title} 
-                onChange={(e) => setTitle(e.target.value)} />
-            </div>
-            <div>
-                <label htmlFor="">Slug</label>
-                <input 
-                type="text" 
-                value={slug} 
-                onChange={(e) => setSlug(e.target.value)} />
-            </div>
+          <div>
+            <label htmlFor="">Title</label>
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="">Slug</label>
+            <input
+              type="text"
+              value={slug}
+              onChange={(e) => setSlug(e.target.value)}
+            />
+          </div>
 
-
-            <input type="submit" />
+          <input type="submit" />
         </div>
       </form>
     </div>
