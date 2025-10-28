76c3f97b73119bda2be59be04cfce1627eb7fabd
bkoimett <benkoimet@gmail.com>
2025-10-28 09:52:44 +0300

    feat(client): add category filter to Book component for data fetching

--- client/src/routes/Book/book.jsx
+++ client/src/routes/Book/book.jsx
@@ -6,11 +6,19 @@ function Book() {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [selectedCategory, setSelectedCategory] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(baseUrl);
+
+        let url = baseUrl;
+        if(selectedCategory) {
+          url += `?category=${selectedCategory}`
+        }
+
+
+        const response = await fetch(url);
 
         if (!response.ok) {
           throw new Error("Failed to fetch data.");
@@ -21,12 +29,12 @@ function Book() {
         setIsLoading(false);
       } catch (error) {
         console.log(error);
-        setError("Error fetching data. Please try again later.")
+        setError("Error fetching data. Please try again later.");
         setIsLoading(false);
       }
     };
     fetchData();
-  }, []);
+  }, [selectedCategory]);
 
   return (
     <div>
@@ -38,6 +46,21 @@ function Book() {
 
       <h2>Fetch Example</h2>
       {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+
+      <div className="filters">
+        <label>Categories</label>
+        <select onChange={(e)=> setSelectedCategory(e.target.value)}>
+          <option value="">All</option>
+          <option value="romance">Romance</option>
+          <option value="science">Science</option>
+          <option value="crime">Crime</option>
+          <option value="food">Food</option>
+          <option value="adventure">Adventure</option>
+          <option value="thriller">Thriller</option>
+          <option value="fiction">Fiction</option>
+          <option value="other">other</option>
+        </select>
+      </div>
 
       {isLoading ? (
         <p>Loading...</p>
